5fc738cd6529e225aa6a849f693a6e80c7d8a206
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-05-10 14:15:30 +0600

    added cookie options

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://modiul-58-db1b5.web.app"],
     credentials: true,
   })
 );
@@ -50,6 +50,12 @@ const client = new MongoClient(uri, {
   },
 });
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -67,18 +73,13 @@ async function run() {
         expiresIn: "1h",
       });
       // console.log(token);
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-        })
-        .send({ status: true });
+      res.cookie("token", token , cookieOptions ).send({ status: true });
     });
 
     // clear cookie when the user Logged Out
     app.post("/logout", async (req, res) => {
       console.log("hello");
-      res.clearCookie("token", { maxAge: 0 }).send({ status: true });
+      res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({ status: true });
     });
 
     //! get all services
